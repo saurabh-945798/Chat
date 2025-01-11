320b398eb884598a30598179a7064c1ea304488b
Saurabh Sharma <132290433+saurabh-945798@users.noreply.github.com>
2025-01-11 11:26:59 +0530

    Update Fotter.jsx

--- src/components/Fotter/Fotter.jsx
+++ src/components/Fotter/Fotter.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
           <span className="ml-3 text-xl">Tailblocks</span>
         </a>
         <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
-          © 2024 saurabh —
+          © 2024  —PAPA
           <a
             href="https://twitter.com/knyttneve"
             className="text-gray-500 ml-1"
